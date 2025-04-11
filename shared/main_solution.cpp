0a2fcd7e88a9851c2609a640d0831ca2f74f53ce
w8385 <w8385@naver.com>
2025-04-11 22:44:20 +0900

    feat: testlib-0.9.41

---
+++ shared/main_solution.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize("O3,unroll-loops")
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define int long long
+#define ll __int128_t
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+auto main() -> signed {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
